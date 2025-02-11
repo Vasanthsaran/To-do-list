2e469ab61a859a2f6467d57e2ac5383b01196673
Your Name <Vasanth>
2025-02-11 13:53:36 +0530

    changed port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import pg from "pg";
 import env from 'dotenv';
 
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 const db = new pg.Client({
   user: process.env.user,
